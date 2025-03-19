5feaabddf7852d2c8844429c89c2ce5a2fe9c718
Fedi <mazgarfedi2003@gmail.com>
2025-03-19 10:37:30 +0100

    Updates

---
+++ static/js/952.8051a8e8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpfe_pro=self.webpackChunkpfe_pro||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.8051a8e8.chunk.js.map
