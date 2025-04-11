1adca32ff3490d59f3c29bd37b0b3a3441ed09c7
Naoures Kraiem <kraiem.nawres@takiacademyteam.com>
2025-04-11 22:54:49 +0100

    some improvements

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { Sun, AlertCircle, Lock, User, Settings, Scale, Search } from "lucide-react"; // Import the search icon
+import {
+  Sun,
+  AlertCircle,
+  Lock,
+  User,
+  Settings,
+  Scale,
+  Search,
+} from "lucide-react"; // Import the search icon
 import "./Home.css";
 import sickIcon from "./assets/image.png";
 import personalIcon from "./assets/image1.png"; // Image remplacée ici
 import casualIcon from "./assets/image2.png"; // Import the new image for Casual
 import vacationIcon from "./assets/image3.png"; // Import the new image for Vacation
+import { DatePicker, Modal, Select, Button } from "antd";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -25,6 +34,11 @@ const Home = () => {
   }, [leaveData]);
 
   const openModal = () => setIsModalOpen(true);
+  const handleOk = () => setIsModalOpen(true);
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const closeModal = () => {
     setIsModalOpen(false);
     setFormData({ fromDate: "", toDate: "", type: "", description: "" });
@@ -55,13 +69,29 @@ const Home = () => {
   const getIcon = (type) => {
     switch (type) {
       case "Vacation":
-        return <img src={vacationIcon} alt="Vacation Icon" className="card-icon sun" />; // Updated icon
+        return (
+          <img
+            src={vacationIcon}
+            alt="Vacation Icon"
+            className="card-icon sun"
+          />
+        ); // Updated icon
       case "Casual":
-        return <img src={casualIcon} alt="Casual Icon" className="card-icon alert" />;
+        return (
+          <img src={casualIcon} alt="Casual Icon" className="card-icon alert" />
+        );
       case "Personal":
-        return <img src={personalIcon} alt="Personal Icon" className="card-icon lock" />;
+        return (
+          <img
+            src={personalIcon}
+            alt="Personal Icon"
+            className="card-icon lock"
+          />
+        );
       case "Sick":
-        return <img src={sickIcon} alt="Sick Icon" className="card-icon medical" />;
+        return (
+          <img src={sickIcon} alt="Sick Icon" className="card-icon medical" />
+        );
       default:
         return null;
     }
@@ -70,7 +100,14 @@ const Home = () => {
   const getLeaveCountByType = (type) => {
     return leaveData.filter((leave) => leave.type === type).length;
   };
+  const { RangePicker } = DatePicker;
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+  };
 
+  const onSearch = (value) => {
+    console.log("search:", value);
+  };
   return (
     <div className="home-container">
       <div className="main-content">
@@ -84,8 +121,14 @@ const Home = () => {
               <span className="menu-dots">⋮</span>
             </div>
             <div className="card-content">
-              <img src={vacationIcon} alt="Vacation Icon" className="card-icon sun" />
-              <span className="card-value">{getLeaveCountByType("Vacation")}</span>
+              <img
+                src={vacationIcon}
+                alt="Vacation Icon"
+                className="card-icon sun"
+              />
+              <span className="card-value">
+                {getLeaveCountByType("Vacation")}
+              </span>
             </div>
           </div>
           <div className="card">
@@ -94,8 +137,14 @@ const Home = () => {
               <span className="menu-dots">⋮</span>
             </div>
             <div className="card-content">
-              <img src={casualIcon} alt="Casual Icon" className="card-icon alert" />
-              <span className="card-value">{getLeaveCountByType("Casual")}</span>
+              <img
+                src={casualIcon}
+                alt="Casual Icon"
+                className="card-icon alert"
+              />
+              <span className="card-value">
+                {getLeaveCountByType("Casual")}
+              </span>
             </div>
           </div>
           <div className="card">
@@ -104,8 +153,14 @@ const Home = () => {
               <span className="menu-dots">⋮</span>
             </div>
             <div className="card-content">
-              <img src={personalIcon} alt="Personal Icon" className="card-icon lock" />
-              <span className="card-value">{getLeaveCountByType("Personal")}</span>
+              <img
+                src={personalIcon}
+                alt="Personal Icon"
+                className="card-icon lock"
+              />
+              <span className="card-value">
+                {getLeaveCountByType("Personal")}
+              </span>
             </div>
           </div>
           <div className="card">
@@ -114,7 +169,11 @@ const Home = () => {
               <span className="menu-dots">⋮</span>
             </div>
             <div className="card-content">
-              <img src={sickIcon} alt="Sick Icon" className="card-icon medical" />
+              <img
+                src={sickIcon}
+                alt="Sick Icon"
+                className="card-icon medical"
+              />
               <span className="card-value">{getLeaveCountByType("Sick")}</span>
             </div>
           </div>
@@ -128,10 +187,16 @@ const Home = () => {
             <thead>
               <tr>
                 <th>
-                  <input type="checkbox" onChange={(e) => {
-                    const checkboxes = document.querySelectorAll(".row-checkbox");
-                    checkboxes.forEach((checkbox) => (checkbox.checked = e.target.checked));
-                  }} />
+                  <input
+                    type="checkbox"
+                    onChange={(e) => {
+                      const checkboxes =
+                        document.querySelectorAll(".row-checkbox");
+                      checkboxes.forEach(
+                        (checkbox) => (checkbox.checked = e.target.checked)
+                      );
+                    }}
+                  />
                 </th>
                 <th>Submission Date</th>
                 <th>From - To</th>
@@ -170,9 +235,7 @@ const Home = () => {
           <h3>Farouk Abichou</h3>
           <p>Software Developer</p>
           <div className="profile-actions">
-            <button className="settings-btn">
-               Settings
-            </button>
+            <button className="settings-btn">Settings</button>
             <button className="view-profile-btn">View profile</button>
           </div>
         </div>
@@ -187,69 +250,114 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <button className="apply-btn" onClick={openModal}>Apply for leave</button>
+        <button className="apply-btn" onClick={openModal}>
+          Apply for leave
+        </button>
       </div>
-
-      {isModalOpen && (
-        <div className="modal-overlay">
-          <div className="modal">
-            <h2>Apply For Leave</h2>
-            <div className="form-group">
-              <label>Leave Date</label>
-              <div className="date-range">
-                <input
-                  type="date"
-                  name="fromDate"
-                  value={formData.fromDate}
-                  onChange={handleInputChange}
-                />
-                <span>to</span>
-                <input
-                  type="date"
-                  name="toDate"
-                  value={formData.toDate}
-                  onChange={handleInputChange}
-                />
-              </div>
-              <p className="hint-text">Select the start and end dates for your leave.</p>
-            </div>
-            <div className="form-group">
-              <label>Type</label>
-              <select
-                name="type"
-                value={formData.type}
-                onChange={handleInputChange}
-              >
-                <option value="">Select...</option>
-                <option>Vacation</option>
-                <option>Casual</option>
-                <option>Personal</option>
-                <option>Sick</option>
-              </select>
-              <p className="hint-text">Select the type of leave.</p>
-            </div>
-            <div className="form-group">
-              <label>Description</label>
-              <textarea
-                name="description"
-                value={formData.description}
-                onChange={handleInputChange}
-                placeholder="Type..."
-              ></textarea>
-            </div>
-            <div className="modal-actions">
-              <button className="cancel-btn" onClick={closeModal}>
-                Cancel
-              </button>
-              <button className="apply-btn" onClick={handleSubmit}>
-                Apply For Leave
-              </button>
-            </div>
-          </div>
+      <Modal
+        title="Apply For Leave"
+        open={isModalOpen}
+        onOk={handleOk}
+        footer={false}
+        onCancel={handleCancel}
+      >
+        <div className="input-container">
+          <label>Leave Date</label>
+          <RangePicker />
         </div>
-      )}
+        <div className="input-container">
+          <label>Type</label>
+          <Select
+            showSearch
+            placeholder="Select a leave type"
+            optionFilterProp="label"
+            onChange={onChange}
+            onSearch={onSearch}
+            options={[
+              {
+                value: "jack",
+                label: "Jack",
+              },
+              {
+                value: "lucy",
+                label: "Lucy",
+              },
+              {
+                value: "tom",
+                label: "Tom",
+              },
+            ]}
+          />
+        </div>
+        <div className="footer-container">
+          <Button>Cancel</Button>
+          <Button type="primary">Apply For Leave</Button>
+        </div>
+      </Modal>
     </div>
   );
 };
 
 export default Home;
+
+// {isModalOpen && (
+//   <div className="modal-overlay">
+//     <div className="modal">
+//       <h2>Apply For Leave</h2>
+//       <div className="form-group">
+//         <label>Leave Date</label>
+//         <div className="date-range">
+//           <RangePicker />
+//           {/* <input
+//             type="date"
+//             name="fromDate"
+//             value={formData.fromDate}
+//             onChange={handleInputChange}
+//           />
+//           <span>to</span>
+//           <input
+//             type="date"
+//             name="toDate"
+//             value={formData.toDate}
+//             onChange={handleInputChange}
+//           /> */}
+//         </div>
+//         <p className="hint-text">
+//           Select the start and end dates for your leave.
+//         </p>
+//       </div>
+//       <div className="form-group">
+//         <label>Type</label>
+//         <select
+//           name="type"
+//           value={formData.type}
+//           onChange={handleInputChange}
+//         >
+//           <option value="">Select...</option>
+//           <option>Vacation</option>
+//           <option>Casual</option>
+//           <option>Personal</option>
+//           <option>Sick</option>
+//         </select>
+//         <p className="hint-text">Select the type of leave.</p>
+//       </div>
+//       <div className="form-group">
+//         <label>Description</label>
+//         <textarea
+//           name="description"
+//           value={formData.description}
+//           onChange={handleInputChange}
+//           placeholder="Type..."
+//         ></textarea>
+//       </div>
+//       <div className="modal-actions">
+//         <button className="cancel-btn" onClick={closeModal}>
+//           Cancel
+//         </button>
+//         <button className="apply-btn" onClick={handleSubmit}>
+//           Apply For Leave
+//         </button>
+//       </div>
+//     </div>
+//   </div>
+// )}
